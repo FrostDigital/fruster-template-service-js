a625a516933659b59c13876479c1d8d5560a895c
Florry <viktor@frostdigital.se>
2017-11-21 11:40:24 +0100

    fixes async/await syntax for app.js

--- app.js
+++ app.js
@@ -10,11 +10,14 @@ const constants = require('./lib/constants');
  * could not be started which most commonly happens if it cannot
  * connect to bus or mongo (if mongo is used).
  */
-service.start(config.bus, config.mongoUrl)
-    .then(() => {
-        log.info(`Successfully started ${constants.SERVICE_NAME}`)
-    })
-    .catch(err => {
+(async function () {
+
+    try {
+        await service.start(config.bus, config.mongoUrl);
+        log.info(`Successfully started ${constants.SERVICE_NAME}`);
+    } catch (err) {
         log.error(`Failed starting ${constants.SERVICE_NAME}`, err);
         process.exit(1);
-    });
+    }
+
+}());
