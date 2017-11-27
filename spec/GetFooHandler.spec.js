4eab75a6c387bc55d991184f7c30064ac871c077
Joel Soderstrom <joel.soderstrom@gmail.com>
2017-11-27 17:28:05 +0530

    format

--- spec/GetFooHandler.spec.js
+++ spec/GetFooHandler.spec.js
@@ -6,17 +6,17 @@ const fixtures = require("./support/fixtures");
 const frusterTemplateService = require("../fruster-template-service");
 
 describe("GetFooHandler", () => {
-	
-	let foo;
 
-	testUtils.startBeforeEach({
-		mockNats: true,
+    let foo;
+
+    testUtils.startBeforeEach({
+        mockNats: true,
         mongoUrl: "mongodb://localhost:27017/fruster-template-service-test",
         service: frusterTemplateService,
-		afterStart: createFoo
-	});
-    
-    it("should return BAD_REQUEST if id is not a uuid", async done => {		
+        afterStart: createFoo
+    });
+
+    it("should return BAD_REQUEST if id is not a uuid", async done => {
         try {
             await bus.request(constants.endpoints.service.GET_FOO, {
                 reqId: "reqId",
@@ -24,14 +24,14 @@ describe("GetFooHandler", () => {
                     id: "fake"
                 }
             });
-        } catch(err) {
-            expect(err.status).toBe(400);    
-            expect(err.error.code).toBe("BAD_REQUEST");    
+        } catch (err) {
+            expect(err.status).toBe(400);
+            expect(err.error.code).toBe("BAD_REQUEST");
             done();
         }
     });
 
-	it("should return NOT_FOUND if Foo does not exist", async done => {		
+    it("should return NOT_FOUND if Foo does not exist", async done => {
         try {
             await bus.request(constants.endpoints.service.GET_FOO, {
                 reqId: "reqId",
@@ -39,14 +39,14 @@ describe("GetFooHandler", () => {
                     id: "26911e7d-bb4c-4a11-a93d-34240993bba2" // <- does not exist
                 }
             });
-        } catch(err) {
-            expect(err.status).toBe(404);    
-            expect(err.error.code).toBe("NOT_FOUND");    
+        } catch (err) {
+            expect(err.status).toBe(404);
+            expect(err.error.code).toBe("NOT_FOUND");
             done();
         }
     });
-    
-    it("should get Foo by its id", async done => {		
+
+    it("should get Foo by its id", async done => {
         const resp = await bus.request(constants.endpoints.service.GET_FOO, {
             reqId: "reqId",
             data: {
@@ -54,10 +54,10 @@ describe("GetFooHandler", () => {
             }
         });
 
-        expect(resp.status).toBe(200);    
-        expect(resp.data).toEqual(foo);    
+        expect(resp.status).toBe(200);
+        expect(resp.data).toEqual(foo);
         done();
-	});
+    });
 
     async function createFoo(connection) {
         const repo = new FooRepo(connection.db);
