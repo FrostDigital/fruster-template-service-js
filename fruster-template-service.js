134687faca916624dbb5b1cc125c7a72862ce9f5
Florry <viktor@frostdigital.se>
2017-12-11 14:30:36 +0100

    fixes some typos, adds some jsDOc and adds docs to subscribe

--- fruster-template-service.js
+++ fruster-template-service.js
@@ -3,14 +3,15 @@ const log = require("fruster-log");
 const mongo = require("mongodb");
 const config = require("./config");
 const constants = require("./lib/constants");
+const docs = require("./lib/docs");
 const FooRepo = require("./lib/repos/FooRepo");
 const GetFooHandler = require("./lib/handlers/GetFooHandler");
 
 module.exports = {
     start: async (busAddress, mongoUrl) => {
         const db = await mongo.connect(mongoUrl);
-        
-        await bus.connect(busAddress);        
+
+        await bus.connect(busAddress);
         await registerHandlers(db);
         await createIndexes(db);
     }
@@ -22,18 +23,20 @@ function registerHandlers(db) {
 
     // HTTP     
     // Add HTTP handlers here
-    
+
     // SERVICE
     bus.subscribe({
         subject: constants.endpoints.service.GET_FOO,
         responseSchema: "GetFooResponse",
         requestSchema: "GetFooRequest",
+        docs: docs.service.GET_FOO,
         handle: (req) => getFooHandler.handle(req)
     })
+
     // Add service handlers here
-}  
+}
 
 
-function createIndexes(db) {    
+function createIndexes(db) {
     // TODO
 }
