b2b7469fdd16a41db0ff603d7f04444616ba1d37
nilsok <nils@frostdigital.se>
2023-01-09 16:49:10 +0100

    fix: update repo, manager and client with new model

--- lib/managers/FooManager.ts
+++ lib/managers/FooManager.ts
@@ -2,6 +2,7 @@ import BarServiceClient from "../clients/BarServiceClient";
 import FooRepo from "../repos/FooRepo";
 import FooModel from "../models/FooModel";
 import errors from "../errors";
+import { FooWithBar } from "../schemas/FooWithBar";
 /**
  * A manager meant to abstract reusable business logic
  * into a nice, friendly API.
@@ -16,18 +17,16 @@ class FooManager {
 	 * @param {String} reqId the fruster request id
 	 * @param {String} id the id of the foo
 	 *
-	 * @return {Promise<FooModel>}
+	 * @return {Promise<FooWithBar>}
 	 */
-	async getFooWithBarById(reqId: string, id: string): Promise<FooModel> {
-		const foo = await this.fooRepo.getById(id);
+	async getFooWithBarById(reqId: string, id: string): Promise<FooWithBar> {
+		const foo = await this.fooRepo.getById(id) as FooWithBar;
 
 		if (!foo)
 			throw errors.notFound(`Foo is not found for id ${id}`);
 
 		if (foo.barId)
-			foo.bar = await BarServiceClient.getBar(reqId, {
-				barId: foo.barId
-			});
+			foo.bar = await (await BarServiceClient.getBar(reqId, {barId: foo.barId})).bar;
 
 		return foo;
 	}
