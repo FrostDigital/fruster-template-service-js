30a28d3eb38e6c59d9211f7af7711b6aefe8152f
nilsok <nils@frostdigital.se>
2023-01-09 16:47:15 +0100

    fix: annotate models and att BarModel and Metadata interfaces

--- lib/models/FooModel.ts
+++ lib/models/FooModel.ts
@@ -1,17 +1,27 @@
+import { Metadata } from "./Metadata";
+
 interface User {
 	id: string;
 }
 
 interface FooModel {
-	id: string;
-	name: string; // Mandatory property
-	description?: string; // Optional property
-	created?: Date;
-	createdBy?: User;
-	updated?: Date;
-	updatedBy?: User;
-	bar?: object;
-	barId?: string;
+
+	/**
+	 * @format uuid
+	 * @description Readonly unique id, is set during creation
+	 */
+	 id: string
+
+	 /*** @description Name of Foo */
+	 name: string
+
+	 /** @description Description of Foo */
+	 description: string
+
+	 /** @description Id of related Bar */
+	 barId?: string,
+
+	 metadata: Metadata
 }
 
 export default FooModel;
